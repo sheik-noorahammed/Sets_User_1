abb720a30cd454fd12aac0bb386422f90cdba979
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-08-29 19:21:36 +0530

    save

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,60 @@
-public class HashSets{
-// Prepare Hashset object here and add person class objects in them iterate it by using for each
+import java.util.HashSet;
+
+// Define the Person class
+class Person {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Override equals and hashCode
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return age == person.age && name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + '}';
+    }
+}
+
+public class HashSets {
+    public static void main(String[] args) {
+        // Create a HashSet of Person objects
+        HashSet<Person> people = new HashSet<>();
+
+        // Add Person objects to the HashSet
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+
+        // Iterate over the HashSet using a for-each loop
+        for (Person person : people) {
+            System.out.println(person);
+        }
+    }
 }
