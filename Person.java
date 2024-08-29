eadb8a2102f6b5f7d03a65e692fc4e89b448d78e
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-08-29 19:28:52 +0530

    save

--- Person.java
+++ Person.java
@@ -1,3 +1,53 @@
-public class Person{
-// prepare a Person Data With Getters and Setters,Constructor,ToString and Override HashCode & Equals Methods in it
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Override toString method
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+
+    // Override equals method
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && name.equals(person.name);
+    }
+
+    // Override hashCode method
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
