5f1e6362398ad2f8039a468d8f8dcf7eaa90b4ac
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-08-29 19:26:08 +0530

    save

--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,54 @@
-public class LinkedHashSets{
-public static void main(String[]args){
-  // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+import java.util.LinkedHashSet;
+import java.util.Iterator;
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return age == person.age && name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + '}';
+    }
 }
+
+public class LinkedHashSets {
+    public static void main(String[] args) {
+        LinkedHashSet<Person> people = new LinkedHashSet<>();
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+        Iterator<Person> iterator = people.iterator();
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            System.out.println(person);
+        }
+    }
+}
+
